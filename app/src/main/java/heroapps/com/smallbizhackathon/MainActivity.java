5a112c442e09695c06644cce38068b6d09d9ab50
Refael Ozeri <refael@hero-apps.io>
2016-11-16 17:04:01 +0200

    first commit

---
+++ app/src/main/java/heroapps/com/smallbizhackathon/MainActivity.java
@@ -0,0 +1,13 @@
+package heroapps.com.smallbizhackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
