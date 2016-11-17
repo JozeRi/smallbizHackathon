077abc0b524be7b70c382682bbde577750271268
MeriSg <sgmerisg@gmail.com>
2016-11-17 10:24:44 +0200

    buttons

--- app/src/main/java/heroapps/com/smallbizhackathon/ui/QuickActions.java
+++ app/src/main/java/heroapps/com/smallbizhackathon/ui/QuickActions.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import heroapps.com.smallbizhackathon.R;
 
@@ -15,13 +15,25 @@ public class QuickActions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_actions);
 
-        ImageButton payimg=(ImageButton)findViewById(R.id.paychecksIV);
-        payimg.setOnClickListener(new View.OnClickListener() {
+
+        Button employe=(Button)findViewById(R.id.emploeyIB);
+        employe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent=new Intent(QuickActions.this,EmployeesActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+            }
+        });
+
+        Button payimg=(Button)findViewById(R.id.payIB);
+        payimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent intent=new Intent(QuickActions.this,EmployeesActivity.class);
+                //startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
             }
         });
