8bb165fc0bdc1bddae71c4751cc484fbc072fe3d
Refael Ozeri <refael@hero-apps.io>
2016-11-17 05:40:04 +0200

    - Change colors

--- app/src/main/java/heroapps/com/smallbizhackathon/ui/SplashActivity.java
+++ app/src/main/java/heroapps/com/smallbizhackathon/ui/SplashActivity.java
@@ -1,6 +1,8 @@
 package heroapps.com.smallbizhackathon.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.ChangeBounds;
@@ -10,6 +12,7 @@ import android.transition.TransitionManager;
 import android.transition.TransitionSet;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -35,6 +38,8 @@ public class SplashActivity extends AppCompatActivity {
   private Scene mBaseScene;
   private Scene mCurrentScene;
 
+  private ImageView mSplashBg;
+
   private ViewGroup mTransitionRoot;
 
   @Override
@@ -50,6 +55,8 @@ public class SplashActivity extends AppCompatActivity {
 
   private void initViews() {
     mTransitionRoot = (ViewGroup) findViewById(R.id.root_container);
+    mSplashBg = (ImageView) findViewById(R.id.splash_bg);
+    populate();
   }
 
   private void checkIfLoggedIn() {
@@ -141,5 +148,19 @@ public class SplashActivity extends AppCompatActivity {
     mBaseScene.enter();
 
 //        postponeEnterTransition();
+  }
+
+  private void populate() {
+    int bgRes = R.drawable.bg_main;
+    Bitmap bgBitmap = getReducedBitmap(bgRes);
+    mSplashBg.setImageBitmap(bgBitmap);
+  }
+
+  private Bitmap getReducedBitmap(int albumArtResId) {
+    // reduce image size in memory to avoid memory errors
+    final BitmapFactory.Options options = new BitmapFactory.Options();
+    options.inJustDecodeBounds = false;
+    options.inSampleSize = 8;
+    return BitmapFactory.decodeResource(getResources(), albumArtResId, options);
   }
 }
