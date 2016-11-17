81365dea533d7752e033ad9ca4b28b0ceb668cf1
MeriSg <sgmerisg@gmail.com>
2016-11-17 09:34:22 +0200

    q activity

---
+++ app/src/main/java/heroapps/com/smallbizhackathon/ui/QuickActions.java
@@ -0,0 +1,30 @@
+package heroapps.com.smallbizhackathon.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import heroapps.com.smallbizhackathon.R;
+
+public class QuickActions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_actions);
+
+        ImageView imageView=(ImageView)findViewById(R.id.paychecksIV);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(QuickActions.this,PaychecksActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+            }
+        });
+
+    }
+}
